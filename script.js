e67cb8096116d800462dc36321d32fbe4cbe7709
Antonia Pettersson <antoniapettersson@Antonias-MacBook-Pro.local>
2021-03-24 16:23:39 +0100

    localStorage

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 console.log("hej");
 let elements = [];
 
+// local Storage
+window.onload = function () {
+  if (JSON.parse(localStorage.getItem("todo-key")) !== null) {
+    elements = JSON.parse(localStorage.getItem("todo-key"));
+    console.log("elements", elements);
+    display();
+  }
+};
+
 // add elements
 function addElement() {
   if (document.querySelector(".addText").value.trim() != "") {
     elements.push(document.querySelector(".addText").value.trim());
     console.log(elements);
-    //alert(elements);
+
+    // local Storage
+    if (localStorage.getItem("todo-key") == null) {
+      localStorage.setItem("todo-key", JSON.stringify(elements));
+    } else {
+      localStorage.setItem("todo-key", JSON.stringify(elements));
+    }
+    console.log(localStorage.getItem("todo-key"));
     display();
   }
 }
@@ -29,6 +45,12 @@ function del(index) {
   console.log("click trash");
   elements.splice(index, 1);
   console.log(elements);
+  // local Storage
+  if (localStorage.getItem("todo-key") == null) {
+    localStorage.setItem("todo-key", JSON.stringify(elements));
+  } else {
+    localStorage.setItem("todo-key", JSON.stringify(elements));
+  }
   display();
 }
 
@@ -39,6 +61,12 @@ function strike(index) {
     elements[index] = elements[index].replace("</strike>", "");
   } else {
     elements[index] = "<strike>" + elements[index] + "</strike>";
+  }
+  // local Storage
+  if (localStorage.getItem("todo-key") == null) {
+    localStorage.setItem("todo-key", JSON.stringify(elements));
+  } else {
+    localStorage.setItem("todo-key", JSON.stringify(elements));
   }
   display();
 }
